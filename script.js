c979554cc37e80063ffccffc936b81b41cad4e36
Leo C <lionsgab24@gmail.com>
2023-10-18 21:50:36 -0300

    ultimo cambio

---
+++ script.js
@@ -0,0 +1,17 @@
+/*INCORPORACION DE API*/
+
+
+function iniciarMap() {
+  var coordenadas = {lat:-34.54693705919758, lng:-58.446754917304986};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: coordenadas,
+    zoom: 10
+  });
+}
+
+var marker = new google.maps.Marker({
+        position: coordenadas,
+        map: map,
+    title: 'El Mas Grande'
+      });
+      
