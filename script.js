3e88f8fc00beeb6f6d791ed4eba96c9c7f0287c7
Leo C <lionsgab24@gmail.com>
2023-10-26 17:44:42 -0300

    Arreglo API

--- script.js
+++ script.js
@@ -14,28 +14,3 @@ var marker = new google.maps.Marker({
         map: map,
     title: 'El Mas Grande'
       });
-
-
-   /*   const axios = require('axios');
-
-const encodedParams = new URLSearchParams();
-encodedParams.set('image_url', 'https://objectcut.com/assets/img/raven.jpg');
-
-const options = {
-  method: 'POST',
-  url: 'https://background-removal.p.rapidapi.com/remove',
-  headers: {
-    'content-type': 'application/x-www-form-urlencoded',
-    'X-RapidAPI-Key': '5774df23e1msh7e396597644e6e0p110050jsn6c52a596e72a',
-    'X-RapidAPI-Host': 'background-removal.p.rapidapi.com'
-  },
-  data: encodedParams,
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
-      */
